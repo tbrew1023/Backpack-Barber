fd7da868430d52363d34a02eea6e8628781f6a76
Trent Brew <tbrew1023@gmail.com>
2018-01-11 19:56:08 -0600

    Setting up the repository for upload

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'home/about'
+
+  get 'home/contact'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
